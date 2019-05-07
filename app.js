448faf7c862eafe5c6f3b498c718d37aeba9694f
Katrina <katrina.lalata@gmail.com>
2019-05-07 10:28:54 -0400

    first renderui commit

---
+++ app.js
@@ -0,0 +1,41 @@
+function onReady() {
+   const toDos = [];
+   const addToDoForm = document.getElementById('addToDoForm');
+
+   function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+      if (!newToDoText.value) { return; }
+      toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+      newToDoText.value = '';
+
+      renderTheUI();
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI();
+    const toDoList = document.getElementById('toDoList');
+
+      toDoList.textContent = '';
+
+      toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+}
+
+window.onload = function() {
+  onReady();
+};
